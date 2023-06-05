a96ab36776d0fb4f9040544f648cd7b8ed0bc4f9
kle-guen <kle-guen@inception.kle-guen.42.fr>
2023-06-05 14:55:53 +0200

    modif

--- Inception/srcs/requirement/mariadb/tools/mariadb.sh
+++ Inception/srcs/requirement/mariadb/tools/mariadb.sh
@@ -4,7 +4,6 @@ service mysql start && mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};" \
 && mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';" \
 && mysql -e "SET PASSWORD FOR '${DB_USER}'@'%'=PASSWORD('${DB_PASSWORD}');" \
 && mysql -e "FLUSH PRIVILEGES;"
-
 mysqld --bind-address=0.0.0.0
 
-exec "$@"
+#exec "$@"
